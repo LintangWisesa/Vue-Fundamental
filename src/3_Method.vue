14fbadcb6e641529cfa2acedb760ae8f44fee905
lintangwisesa <lintangwisesa@ymail.com>
2019-01-06 18:26:07 +0700

    vue fundamental

---
+++ src/3_Method.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ sambutan() }}</h1>
+    <h1>{{ salam() }}</h1>
+    <h1>Halo {{ nama }} ({{ usia }})</h1>   
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      nama: 'Andi',
+      usia: 23,
+      waktu: 'malam'
+    }
+  },
+  methods: {
+    sambutan(){
+      return 'Selamat datang!'
+    },
+    salam(){
+      return `Selamat ${this.waktu}, ${this.nama}`
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
