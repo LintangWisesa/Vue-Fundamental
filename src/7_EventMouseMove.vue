1d42e7c46bcc6a5e72c3835b5c8c5a3da8898444
lintangwisesa <lintangwisesa@ymail.com>
2019-01-06 18:26:54 +0700

    vue fundamental

---
+++ src/7_EventMouseMove.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-on:mousemove="posisi">
+    <h1>Posisi Kursor</h1>
+    <h1>X: {{ x }} | Y: {{ y }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      x: 0,
+      y: 0
+    }
+  },
+  methods: {
+    posisi(e){
+      this.x = e.clientX
+      this.y = e.clientY
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/8_EventKeyboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Input Keyboard</h1>
+    <input v-on:keyup="keyboard" type="text" placeholder="Ketik nama Anda..."/>
+    <h1>{{ input }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      input: ''
+    }
+  },
+  methods: {
+    keyboard(e){
+      console.log(e)
+      this.input += e.key
+    }
+  }
+}
+</script>
+
+<style>
+</style>
