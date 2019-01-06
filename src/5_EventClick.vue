52b16348fa2eb8e64f7f561e397de8a0eeab4751
lintangwisesa <lintangwisesa@ymail.com>
2019-01-06 18:26:38 +0700

    vue fundamental

---
+++ src/5_EventClick.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+     <h1>{{ angka }}</h1>
+     <button v-on:click="angka++">+ Tambah</button>
+     &nbsp;
+     <button v-on:click="angka--">- Kurang</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      angka: 0
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/6_EventDblClick.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+     <h1>{{ angka }}</h1>
+     <button v-on:click="tambah">+ Tambah</button>
+     &nbsp;
+     <button v-on:click="kurang">- Kurang</button>
+     <br/><br/>
+     <button v-on:dblclick="reset">Double click untuk Reset!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      angka: 0
+    }
+  },
+  methods: {
+    tambah(){
+      this.angka++
+    },
+    kurang(){
+      this.angka--
+    },
+    reset(){
+      this.angka = 0
+    }
+  }
+}
+</script>
+
+<style>
+</style>
