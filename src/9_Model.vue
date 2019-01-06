39e8d3872da8c0a600a9503408147239b8d93820
lintangwisesa <lintangwisesa@ymail.com>
2019-01-06 18:27:06 +0700

    vue fundamental

---
+++ src/9_Model.vue
@@ -0,0 +1,22 @@
+// 2 ways data binding
+
+<template>
+  <div>
+    <h1>Input Keyboard</h1>
+    <input v-model="input" type="text" placeholder="Ketik nama Anda..."/>
+    <h1>{{ input }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      input: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
