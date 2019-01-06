91ae44007f07214e42a12bc1d6e7ce9804907d2a
lintangwisesa <lintangwisesa@ymail.com>
2019-01-06 18:25:51 +0700

    vue fundamental

---
+++ src/2_Data.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Halo {{ nama }} ({{ usia }})</h1>   
+  </div>
+</template>
+
+<script>
+export default {
+  // data: function(){
+  //   return {
+  //     nama: 'Andi',
+  //     usia: 23
+  //   }
+  // }
+  data(){
+    return {
+      nama: 'Andi',
+      usia: 23
+    }
+  }
+}
+</script>
+
+<style>
+</style>
