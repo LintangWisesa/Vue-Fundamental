307033c8baa4054e318ce1c11bb17de1a7daf010
lintangwisesa <lintangwisesa@ymail.com>
2019-01-06 18:27:20 +0700

    vue fundamental

---
+++ src/20_AxiosGet.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button v-on:click="get">Klik</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  methods: {
+    get(){
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((x)=>{
+        console.log(x.data)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
