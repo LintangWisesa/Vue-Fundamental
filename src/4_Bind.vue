3c75441212a60c388572e5a73d1d07087ffd9bd0
lintangwisesa <lintangwisesa@ymail.com>
2019-01-06 18:26:22 +0700

    vue fundamental

---
+++ src/4_Bind.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+     <a v-bind:href="web">Klik di sini</a>
+     <br/>
+     <a :href="web">Di sini aja!</a>
+     <br/>
+     <div v-html="webTag"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      web: 'https://www.cgv.id',
+      webTag: '<a href="https://www.cgv.id">Sini juga bisa...</a>'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
