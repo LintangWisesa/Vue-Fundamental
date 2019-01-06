bb18abd03373223be0c2dbddb39f76c585ea8cba
lintangwisesa <lintangwisesa@ymail.com>
2019-01-06 18:25:34 +0700

    vue fundamental

---
+++ src/1_Style.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Halo Dunia</h1>   
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  body {
+    background-color: lightcyan
+  }
+  h1 {
+    color: red
+  }
+</style>
