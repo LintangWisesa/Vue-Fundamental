ea8c055f91b1d47470ee3d92496e31189c469d4d
lintangwisesa <lintangwisesa@ymail.com>
2019-01-07 16:38:00 +0700

    vue fundamental

---
+++ src/17b_Receive_Props.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <hr>
+    <h1>Pesan dari Parent:</h1>
+    <h3>{{ pesan }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['pesan']
+}
+</script>
+
+<style>
+</style>
