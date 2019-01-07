c344e5a55362e7ae362db20ea1c7d3e91f01a7e8
lintangwisesa <lintangwisesa@ymail.com>
2019-01-07 10:53:47 +0700

    vue fundamental

---
+++ src/13_Array_Loop.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Array Looping</h1>
+
+    <h3>Characters:</h3>
+    <ul>
+      <li v-for="x in characters">
+        {{ x }}
+      </li>
+    </ul>
+
+    <h3>Users:</h3>
+    <ul>
+      <div v-for="(value, index) in users">
+        {{ index }}. {{ value.name }} ({{ value.age}}) 
+      </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      characters: ['Nobita', 'Doraemon', 'Shizuka', 'Giant', 'Suneo'],
+      users: [
+        {name: 'Andi', age: 21},
+        {name: 'Budi', age: 22},
+        {name: 'Caca', age: 23},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
